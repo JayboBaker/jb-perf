8cb138f8c4d49a3b399fda25181b6c77c7a25b41
James Baker <james.baker@crunch.co.uk>
2017-06-04 12:03:16 +0100

    JB - Add required props to labelled input component. Pass props to children

--- src/components/presentational/molecules/labelled-input/LabelledInput.js
+++ src/components/presentational/molecules/labelled-input/LabelledInput.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Label from '../../atoms/label/Label'
 import Input from '../../atoms/input/Input'
 
-const LabelledInput = () =>
-  <Label>
-    <Input />
+const LabelledInput = ({name, labelText}) =>
+  <Label text={labelText} htmlFor={name}>
+    <Input {...{name}} />
   </Label>
+
+LabelledInput.propTypes = {
+  labelText: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
+}
 
 export default LabelledInput
--- src/components/presentational/molecules/labelled-input/LabelledInput.spec.js
+++ src/components/presentational/molecules/labelled-input/LabelledInput.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import {expect} from 'chai'
+import { mount } from 'enzyme'
+import { expect } from 'chai'
 
 import Component from './LabelledInput'
 
@@ -8,11 +8,24 @@ import Label from '../../atoms/label/Label'
 import Input from '../../atoms/input/Input'
 
 describe('<SearchBar />', () => {
-  const wrapper = shallow(<Component />)
+  const initialProps = {
+    name: 'test-search-bar-name',
+    labelText: 'Test Label Text'
+  }
+  const wrapper = mount(<Component {...initialProps} />)
+
   it('contains a <Label /> component', function () {
     expect(wrapper.find(Label)).to.have.length(1)
   })
   it('contains a <Input /> component', function () {
     expect(wrapper.find(Input)).to.have.length(1)
+  })
+  it('passes props to the label element', function () {
+    expect(wrapper.find('label').props().htmlFor).to.equal(initialProps.name)
+    expect(wrapper.find('label').text()).to.equal(initialProps.labelText)
+  })
+  it('passes props to the input element', function () {
+    expect(wrapper.find('input').props().name).to.equal(initialProps.name)
+    expect(wrapper.find('input').props().id).to.equal(initialProps.name)
   })
 })
