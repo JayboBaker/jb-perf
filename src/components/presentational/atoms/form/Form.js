ea58f2302d87d4bf475e396952a24859a5186bf1
James Baker <james.baker@crunch.co.uk>
2017-06-04 10:43:43 +0100

    JB - Add render of children to form

--- src/components/presentational/atoms/form/Form.js
+++ src/components/presentational/atoms/form/Form.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Form = () =>
-  <form />
+const Form = ({children}) =>
+  <form>
+    {children}
+  </form>
 
+Form.propTypes = {
+  children: PropTypes.array.isRequired
+}
 export default Form
--- src/components/presentational/atoms/form/Form.spec.js
+++ src/components/presentational/atoms/form/Form.spec.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import {expect} from 'chai'
 
 import Component from './Form'
 
+const initialProps = {
+  children: <div className='test-form-children'>Test</div>
+}
 describe('<Form />', () => {
+  const wrapper = mount(<Component {...initialProps} />)
   it('contains an <form/> element', function () {
-    const wrapper = shallow(<Component />)
     expect(wrapper.find('form')).to.have.length(1)
+  })
+
+  it('renders its children', function () {
+    expect(wrapper.find('.test-form-children')).to.have.length(1)
   })
 })
