fa3520bb6f62d797345becbd2c1983fa7bdada51
James Baker <james.baker@crunch.co.uk>
2017-06-04 10:44:49 +0100

    JB - Add children to SearchBar component

--- src/components/presentational/molecules/search-bar/SearchBar.js
+++ src/components/presentational/molecules/search-bar/SearchBar.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
+import Form from '../../atoms/form/Form'
+import Input from '../../atoms/input/Input'
+import Button from '../../atoms/button/Button'
+
 const SearchBar = () =>
-  <div />
+  <div>
+    <Form>
+      <Input />
+      <Button />
+    </Form>
+  </div>
 
 export default SearchBar
--- src/components/presentational/molecules/search-bar/SearchBar.spec.js
+++ src/components/presentational/molecules/search-bar/SearchBar.spec.js
@@ -4,9 +4,22 @@ import {expect} from 'chai'
 
 import Component from './SearchBar'
 
+import Form from '../../atoms/form/Form'
+import Input from '../../atoms/input/Input'
+import Button from '../../atoms/button/Button'
+
 describe('<SearchBar />', () => {
-  it('contains an <div/> element', function () {
-    const wrapper = shallow(<Component />)
+  const wrapper = shallow(<Component />)
+  it('contains a <div /> element', function () {
     expect(wrapper.find('div')).to.have.length(1)
+  })
+  it('contains a <Form /> component', function () {
+    expect(wrapper.find(Form)).to.have.length(1)
+  })
+  it('contains a <Button /> component', function () {
+    expect(wrapper.find(Button)).to.have.length(1)
+  })
+  it('contains an <Input /> component', function () {
+    expect(wrapper.find(Input)).to.have.length(1)
   })
 })
