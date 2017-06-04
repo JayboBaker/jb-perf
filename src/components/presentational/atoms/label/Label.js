7fa07cc0c60e3cf5af1f0d2aa1aa367419e8345b
James Baker <james.baker@crunch.co.uk>
2017-06-04 11:59:34 +0100

    JB - Add required props to input

--- src/components/presentational/atoms/label/Label.js
+++ src/components/presentational/atoms/label/Label.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Label = ({children}) =>
-  <label>
+const Label = ({text, htmlFor, children}) =>
+  <label htmlFor={htmlFor}>
+    {text}
     {children}
   </label>
 
 Label.propTypes = {
+  text: PropTypes.string.isRequired,
+  htmlFor: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([
     PropTypes.array,
     PropTypes.object
   ]).isRequired
 }
+
 export default Label
--- src/components/presentational/atoms/label/Label.spec.js
+++ src/components/presentational/atoms/label/Label.spec.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import {expect} from 'chai'
 
 import Component from './Label'
 
-const initialProps = {
-  children: <div className='test-label-children'>Test</div>
-}
 describe('<Label />', () => {
-  const wrapper = mount(<Component {...initialProps} />)
+  const initialProps = {
+    children: <div className='test-label-children' />,
+    htmlFor: 'test-html-for',
+    text: 'Test Label Text'
+  }
+  const wrapper = shallow(<Component {...initialProps} />)
+
   it('contains an <label/> element', function () {
     expect(wrapper.find('label')).to.have.length(1)
   })
 
   it('renders its children', function () {
     expect(wrapper.find('.test-label-children')).to.have.length(1)
+  })
+
+  it('passes props to the label element', function () {
+    expect(wrapper.find('label').props().htmlFor).to.equal(initialProps.htmlFor)
+    expect(wrapper.find('label').text()).to.equal(initialProps.text)
   })
 })
